f1067c36e604aa32b780af6e3a15fc9b21c186a2
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2023-12-14 11:52:57 +0100

    Feat - Add status filter

--- app/controllers/api_viewer_controller.rb
+++ app/controllers/api_viewer_controller.rb
@@ -1,12 +1,23 @@
 class ApiViewerController < ApplicationController
+  before_action :load_filtered_data, only: :index
+
   def index
-    country = params[:country]
-    filtered_data = cache_answer.select { |entry| entry['currentCountry'] == country } if country.presence
-    @pagy, @vessels = pagy_array(filtered_data || cache_answer)
+    @pagy, @vessels = pagy_array(@filtered_data)
     @country_filter = cache_country_filter
+    @status_filter = cache_status_filter
   end
 
   private
+
+  def load_filtered_data
+    @filtered_data = cache_answer
+    apply_filters
+  end
+
+  def apply_filters
+    @filtered_data = @filtered_data.select { |entry| entry['currentCountry'] == params[:country] } if params[:country].present?
+    @filtered_data = @filtered_data.select { |entry| entry['vesselStatus'] == params[:status] } if params[:status].present?
+  end
 
   def cache_answer
     Rails.cache.fetch("api_response", expires_in: 12.hours) do
@@ -17,6 +28,12 @@ class ApiViewerController < ApplicationController
   def cache_country_filter
     Rails.cache.fetch("api_country_filter", expires_in: 12.hours) do
       cache_answer.pluck('currentCountry').uniq.compact.sort
+    end
+  end
+
+  def cache_status_filter
+    Rails.cache.fetch("api_status_filter", expires_in: 12.hours) do
+      cache_answer.pluck('vesselStatus').uniq.compact.sort
     end
   end
 
